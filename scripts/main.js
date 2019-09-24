c5f40fd43fd50ddabdd7154e1cdd0adc21b92774
Sergio <Plojka44@gmail.com>
2019-09-24 17:15:16 +0200

    add buttom two and thre

--- scripts/main.js
+++ scripts/main.js
@@ -17,12 +17,32 @@ function myFunction() {
   }
 
 //button of myAudio
-var oAudio = document.getElementById('myAudio');
-function playAudio() {  
+function playAudio() {
+  var oAudio = document.getElementById('myAudio');  
   if (oAudio.paused) { 
       oAudio.play(); 
   } 
   else { 
       oAudio.pause();  
   } 
-}                
+}   
+
+function playAudio_two() {
+  var oAudio_two = document.getElementById('myAudio_two');  
+  if (oAudio_two.paused) { 
+      oAudio_two.play(); 
+  } 
+  else { 
+      oAudio_two.pause();  
+  } 
+} 
+
+function playAudio_thre() {
+  var oAudio_thre = document.getElementById('myAudio_thre');  
+  if (oAudio_thre.paused) { 
+      oAudio_thre.play(); 
+  } 
+  else { 
+      oAudio_thre.pause();  
+  } 
+} 
